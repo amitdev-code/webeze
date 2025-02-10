bbac6a9b14ee702508c91b14de638cd992ea91bc
amikum1997 <ams281997@gmail.com>
2025-02-10 16:59:24 +0530

    update

--- frontend-app/src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-// rootReducer.ts
-import { combineReducers } from '@reduxjs/toolkit';
-import authReducer from './slice/authSlice'; // Example slice
-
-const rootReducer = combineReducers({
-  auth: authReducer, // Add other slices here
-});
-
-export default rootReducer;
